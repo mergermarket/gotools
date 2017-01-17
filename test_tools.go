3f33639f7e069fbe58f5f0b0e162a5ad4adaa9af
stevep-mm <steve.prosser@mergermarket.com>
2017-01-17 16:27:14 +0000

    @quii, @stevep-mm: add random string generator

--- test_tools.go
+++ test_tools.go
@@ -1,10 +1,27 @@
 package tools
 
-import "testing"
+import (
+	"testing"
+	"time"
+	"math/rand"
+)
 
 // NewTestTools returns a logger and statsd instance to use in your tests
 func NewTestTools(t *testing.T) (logger, StatsD) {
 	logger := TestLogger{T: t}
 	statsd, _ := NewStatsD(NewStatsDConfig(false, logger))
 	return logger, statsd
+}
+
+// RandomString creates a random string of alphanumeric characters of length strlen
+// from: https://siongui.github.io/2015/04/13/go-generate-random-string/
+// there are faster ways: http://stackoverflow.com/questions/22892120/how-to-generate-a-random-string-of-a-fixed-length-in-golang
+func RandomString(strlen int) string {
+	rand.Seed(time.Now().UTC().UnixNano())
+	const chars = "abcdefghijklmnopqrstuvwxyz0123456789"
+	result := make([]byte, strlen)
+	for i := 0; i < strlen; i++ {
+		result[i] = chars[rand.Intn(len(chars))]
+	}
+	return string(result)
 }
