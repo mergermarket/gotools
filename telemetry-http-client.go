0a727dc5d0353dbf64fbac470d1c75c7a5293d5f
John Muth <john.muth@mergermarket.com>
2017-01-23 15:34:22 +0000

    http client with statsd - 'http_client.response_time_ms' histogram only for now

---
+++ telemetry-http-client.go
@@ -0,0 +1,58 @@
+package tools
+
+import (
+	"net/http"
+	"time"
+	"fmt"
+	"io"
+)
+
+type TelemetryHttpClient interface {
+	Do(r *http.Request) (*http.Response, error)
+	Get(url string) (*http.Response, error)
+	Post(url string, bodyType string, body io.Reader) (*http.Response, error)
+}
+
+type clock interface {
+	Now() time.Time
+}
+
+type telemetryHTTPClient struct {
+	httpClient *http.Client
+	statsd StatsD
+	clock clock
+	callee string
+}
+
+const responseTimeKey = "http_client.response_time_ms"
+
+func (thc *telemetryHTTPClient) Do(r *http.Request) (*http.Response, error) {
+	start := thc.clock.Now()
+	resp, err := thc.httpClient.Do(r)
+	finish := thc.clock.Now()
+	duration := (finish.Nanosecond()-start.Nanosecond())/1000000
+	thc.statsd.Histogram(responseTimeKey, float64(duration), "http_callee:"+thc.callee, "method:"+r.Method, fmt.Sprintf("resp_status:%d",resp.StatusCode))
+	return resp, err
+}
+
+func (thc *telemetryHTTPClient) Get(url string) (resp *http.Response, err error) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return resp, err
+	}
+	return thc.Do(req)
+}
+
+func (thc *telemetryHTTPClient) Post(url string, bodyType string, body io.Reader) (resp *http.Response, err error) {
+	req, err := http.NewRequest("POST", url, body)
+	if err != nil {
+		return resp, err
+	}
+	req.Header.Set("Content-Type", bodyType)
+	return thc.Do(req)
+}
+
+
+func TelemetryHTTPClient(client *http.Client, statsd StatsD, clock clock, callee string) TelemetryHttpClient {
+	return &telemetryHTTPClient{statsd:statsd, httpClient:client, clock:clock, callee:callee}
+}
---
+++ telemetry-http-client_test.go
@@ -0,0 +1,104 @@
+package tools
+
+import (
+	"testing"
+	"net/http"
+	"net/http/httptest"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"time"
+	"strings"
+)
+
+type fakeClock struct {
+	times chan time.Time
+}
+
+func (f *fakeClock) Now() time.Time {
+	return <-f.times
+}
+
+func TestTelemetryHTTPClient_Do(t *testing.T) {
+
+	fc := &fakeClock{make(chan time.Time, 2)}
+
+	msd := &MockStatsD{}
+	hc := http.DefaultClient
+	wc := TelemetryHTTPClient(hc, msd, fc, "my-remote-service")
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello World")
+	}))
+	defer ts.Close()
+
+	start := time.Now()
+	fc.times <- start
+	fc.times <- start.Add(100 * time.Millisecond)
+
+	req, _ := http.NewRequest("GET", ts.URL, nil)
+
+	assert.Len(t, msd.calls, 0)
+
+	resp, _ := wc.Do(req)
+
+	assert.Len(t, msd.calls, 1)
+	assert.NotNil(t, resp)
+	assert.Equal(t, "Histogram", msd.calls[0].Method)
+	assert.Equal(t, 100.0, msd.calls[0].Args.Value)
+	assert.Equal(t, []string{"http_callee:my-remote-service", "method:GET", "resp_status:200"}, msd.calls[0].Args.Tags)
+
+}
+
+func TestTelemetryHTTPClient_Get(t *testing.T) {
+
+	fc := &fakeClock{make(chan time.Time, 2)}
+
+	msd := &MockStatsD{}
+	hc := http.DefaultClient
+	wc := TelemetryHTTPClient(hc, msd, fc, "my-remote-service")
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello World")
+	}))
+	defer ts.Close()
+
+	start := time.Now()
+	fc.times <- start
+	fc.times <- start.Add(100 * time.Millisecond)
+
+	assert.Len(t, msd.calls, 0)
+
+	resp, _ := wc.Get(ts.URL)
+
+	assert.Len(t, msd.calls, 1)
+	assert.NotNil(t, resp)
+	assert.Equal(t, "Histogram", msd.calls[0].Method)
+	assert.Equal(t, 100.0, msd.calls[0].Args.Value)
+	assert.Equal(t, []string{"http_callee:my-remote-service", "method:GET", "resp_status:200"}, msd.calls[0].Args.Tags)
+
+}
+
+func TestTelemetryHTTPClient_Post(t *testing.T) {
+	fc := &fakeClock{make(chan time.Time, 2)}
+
+	msd := &MockStatsD{}
+	hc := http.DefaultClient
+	wc := TelemetryHTTPClient(hc, msd, fc, "my-remote-service")
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello World")
+	}))
+	defer ts.Close()
+
+	start := time.Now()
+	fc.times <- start
+	fc.times <- start.Add(100 * time.Millisecond)
+
+	assert.Len(t, msd.calls, 0)
+
+	resp, _ := wc.Post(ts.URL, "application/json", strings.NewReader(`{"hello":"world"}`))
+
+	assert.Len(t, msd.calls, 1)
+	assert.NotNil(t, resp)
+	assert.Equal(t, "Histogram", msd.calls[0].Method)
+	assert.Equal(t, 100.0, msd.calls[0].Args.Value)
+	assert.Equal(t, []string{"http_callee:my-remote-service", "method:POST", "resp_status:200"}, msd.calls[0].Args.Tags)
+
+}
