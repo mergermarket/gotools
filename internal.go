62e4cb5ba0ab1bd6d11b7561002565ac5716655f
fahri cavusoglu <fahric@hotmail.co.uk>
2017-12-05 15:27:53 +0000

    add internal/runtime-info function

--- internal.go
+++ internal.go
@@ -3,6 +3,8 @@ package tools
 import (
 	"fmt"
 	"net/http"
+	"runtime"
+	"encoding/json"
 )
 
 // InternalHealthCheck is used by our infrastructure to check the service is listening
@@ -17,4 +19,22 @@ func InternalLogConfig(config interface{}, logger logger) http.HandlerFunc {
 		logger.Info(fmt.Sprintf("Application config - %+v", config))
 		fmt.Fprint(w, "Logged the config")
 	}
+}
+
+// InternalLRuntimeInfo logs current running version of GO
+func InternalRuntimeInfo(w http.ResponseWriter, r *http.Request) {
+	versionInfo := struct {
+		Platform string `json:"platform"`
+		Version  string `json:"version"`
+	}{
+		"go",
+		runtime.Version(),
+	}
+	versionInfoBytes, err := json.Marshal(versionInfo)
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte("Error converting runtime info to JSON"))
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(versionInfoBytes)
 }
