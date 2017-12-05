bc34ca4b06e408f44c120192baa5b45a79f54494
Fahri Cavusoglu <fahri.cavusoglu@mergermarket.com>
2017-12-05 15:37:11 +0000

    add internal/runtime-info function (#18)

--- internal_test.go
+++ internal_test.go
@@ -18,3 +18,16 @@ func TestInternalHealthcheckRouter(t *testing.T) {
 		t.Error("Expected a 200 but I got ", response.StatusCode)
 	}
 }
+
+func TestInternalLRuntimeInfo(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(InternalRuntimeInfo))
+	response, err := http.Get(server.URL + "/internal/runtime-info")
+
+	if err != nil {
+		t.Fatal("I got an error requesting runtime-info ", err)
+	}
+
+	if response.StatusCode != http.StatusOK {
+		t.Error("Expected a 200 but I got ", response.StatusCode)
+	}
+}
